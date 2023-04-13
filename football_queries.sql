a20bff7de46b3c3b2eaf398a6e1c88e899563d9d
Subrina7 <Subrinaf.7@gmail.com>
2023-04-13 22:55:18 +0100

    almost completed questionsgit add .

--- football_queries.sql
+++ football_queries.sql
@@ -1,3 +1,33 @@
+-- q1
 -- SELECT * FROM matches WHERE season = 2017; 
---SELECT * FROM matches where hometeam = 'Barcelona';
-SELECT * FROM matches WHERE (awayteam = 'Barcelona' OR hometeam = 'Barcelona');
+
+-- q2
+-- SELECT * FROM matches where hometeam = 'Barcelona';
+-- SELECT * FROM matches Where awayteam = 'Barcelona';
+-- SELECT * FROM matches 
+--WHERE (awayteam = 'Barcelona' OR hometeam = 'Barcelona');
+
+-- q3
+-- SELECT * FROM divisions WHERE country = ('Scotland');
+
+-- q4
+-- SELECT DISTINCT code 
+-- from divisions 
+-- WHERE name LIKE '%Bundesliga%';
+
+-- SELECT COUNT(*) 
+-- FROM matches 
+-- WHERE (division_code = 'D1' OR division_code = 'D2')
+-- AND (hometeam = 'Freiburg' OR awayteam = 'Freiburg');
+
+
+-- q5
+-- SELECT * FROM matches where (hometeam LIKE '%City%' OR awayteam LIKE '%City%');
+
+-- q6
+-- SELECT code FROM divisions WHERE country = 'France';
+-- SELECT COUNT(DISTINCT hometeam) + COUNT(DISTINCT awayteam) FROM matches WHERE (division_code = 'F1' OR division_code ='F2');
+
+-- q7 
+SELECT * FROM matches WHERE (hometeam = 'Huddersfield' AND awayteam = 'Swansea') OR (hometeam = 'Swansea' AND awayteam = 'Huddersfield');
+
