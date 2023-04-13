84f61eaf3930fcc01cd3ea3ad5650debb78d449d
Subrina7 <Subrinaf.7@gmail.com>
2023-04-13 16:43:01 +0100

    question2 complete

--- football_queries.sql
+++ football_queries.sql
@@ -1,1 +1,3 @@
-SELECT * FROM matches WHERE season = 2017; 
+-- SELECT * FROM matches WHERE season = 2017; 
+--SELECT * FROM matches where hometeam = 'Barcelona';
+SELECT * FROM matches WHERE (awayteam = 'Barcelona' OR hometeam = 'Barcelona');
